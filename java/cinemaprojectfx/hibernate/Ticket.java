8992304407e687a359bdb9b2f0c79db37f10bb9f
wsxx22 <46091789+wsxx22@users.noreply.github.com>
2019-01-04 14:45:19 +0100

    Test

--- java/cinemaprojectfx/hibernate/Ticket.java
+++ java/cinemaprojectfx/hibernate/Ticket.java
@@ -2,13 +2,5 @@ package cinemaprojectfx.hibernate;
 
 public class Ticket {
 
-    private int test2;
 
-    Ticket () {
-        setTest2(1);
-    }
-
-    public void setTest2(int test2) {
-        this.test2 = test2;
-    }
 }
